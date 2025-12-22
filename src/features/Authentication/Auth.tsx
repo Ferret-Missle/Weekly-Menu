5cb351e7b61f918eae4da5bc776efdd1798e6092
Ferret-Missle <shosan.918@gmail.com>
2025-12-22 11:15:25 +0900

    theme

--- src/features/Authentication/Auth.tsx
+++ src/features/Authentication/Auth.tsx
@@ -1,25 +1,19 @@
 import React from 'react';
 
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-
 import { LoginTypography } from './components/AuthText';
 import { AuthWrapper } from './components/AuthWrapper';
+import { Form } from './components/Form';
 
-const Auth: React.FC = () => {
+export const Auth: React.FC = () => {
 	return (
 		<AuthWrapper>
 			<>
 				<LoginTypography role="title">献立スケジュール</LoginTypography>
-				<LoginTypography role="subtitle">週間献立プランとレシピ、買い物リストを管理</LoginTypography>
+				<LoginTypography role="subtitle">
+					週間献立プランとレシピ、買い物リストを管理
+				</LoginTypography>
 			</>
-			<Box sx={{ mt: 3 }}>
-				<Typography variant="body2" color="text.secondary" align="center">
-					テーマを切り替える：
-				</Typography>
-			</Box>
+			<Form />
 		</AuthWrapper>
 	);
 };
-
-export default Auth;
---
+++ src/features/Authentication/components/Form.tsx
@@ -0,0 +1,5 @@
+import { LoginTypography } from './AuthText';
+
+export const Form: React.FC = () => {
+	return <LoginTypography role="caption">メールアドレス</LoginTypography>;
+};
