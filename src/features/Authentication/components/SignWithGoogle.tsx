f999dc11f07d63acd6dcc6068983181090b3bb87
Ferret-Missle <shosan.918@gmail.com>
2025-12-30 17:10:36 +0900

    reseach: sign Google

--- src/features/Authentication/components/SignWithGoogle.tsx
+++ src/features/Authentication/components/SignWithGoogle.tsx
@@ -1,17 +1,17 @@
-// import { signInWithPopup } from 'firebase/auth';
+import { signInWithPopup } from "firebase/auth";
 
 import IconButton from "@mui/material/IconButton";
 import Stack from "@mui/material/Stack";
 
 import googleIcon from "../../../assets/google.svg";
-// import { auth, provider } from '../../../providers/firebase';
+import { auth, provider } from "../../../providers/firebase";
 import { LoginTypography } from "../../../styles/AuthText";
 
 export const SignWithGoogle: React.FC<{ mode: string }> = ({ mode }) => {
 	return (
 		<Stack spacing={1} sx={{ alignItems: "center", width: "auto" }}>
 			<IconButton
-				// onClick={signInWithGoogle}
+				onClick={signInWithGoogle}
 				sx={{ bgcolor: "white", border: "0.5px solid" }}
 			>
 				<img src={googleIcon} alt="Google Icon" />
@@ -23,11 +23,11 @@ export const SignWithGoogle: React.FC<{ mode: string }> = ({ mode }) => {
 	);
 };
 
-// const signInWithGoogle = async () => {
-// 	try {
-// 		await signInWithPopup(auth, provider);
-// 	} catch (error: unknown) {
-// 		console.log(error);
-// 		return error;
-// 	}
-// };
+const signInWithGoogle = async () => {
+	// try {
+	await signInWithPopup(auth, provider);
+	// } catch (error: unknown) {
+	// 	console.log(error);
+	// 	return error;
+	// }
+};
