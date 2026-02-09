848a248eb5f824dd6740fc842a2ee1fd76781eb6
Ferret-Missle <shosan.918@gmail.com>
2026-02-09 23:31:52 +0900

    feat: uncomment CalendarContentsHeader in Calendar component; remove unused PlanSelectionTab

--- src/features/calendar/Calendar.tsx
+++ src/features/calendar/Calendar.tsx
@@ -3,11 +3,12 @@ import Box from "@mui/material/Box";
 import { ContentsWrapper } from "../common/views/ContentsWrapper";
 import { WeekPeriodBar } from "./views/weekPeriodBar";
 import { SavingChangeFAB } from "./views/SavingChangeFAB";
+import { CalendarContentsHeader } from "./views/CalendarContentsHeader";
 
 export const Calendar = () => {
 	return (
 		<ContentsWrapper>
-			{/* <CalendarContentsHeader /> */}
+			<CalendarContentsHeader />
 			<WeekPeriodBar />
 			<Box>//todo: button of move to this week</Box>
 			<Box sx={{ height: "2000px" }}>//todo: card of plans</Box>
--- src/features/calendar/views/CalendarContentsHeader.tsx
+++ src/features/calendar/views/CalendarContentsHeader.tsx
@@ -2,7 +2,3 @@ import Box from "@mui/material/Box";
 export const CalendarContentsHeader = () => {
 	return <Box sx={{ backgroundColor: "white" }}>sticky test</Box>;
 };
-
-const PlanSelectionTab = () => {
-	return;
-};
