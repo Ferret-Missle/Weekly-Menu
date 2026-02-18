b85d117a3b54cc6880b7ba0b08fd0f47c4503646
Ferret-Missle <shosan.918@gmail.com>
2026-02-19 00:11:12 +0900

    feat: update date handling in showDateString and CardsList components

--- src/features/calendar/composable/showDateString.ts
+++ src/features/calendar/composable/showDateString.ts
@@ -1,4 +1,4 @@
-export function showDateString(dateStr: string) {
+export function showDateString(dateStr: Date) {
 	const date = new Date(dateStr);
 	const dayOfWeek = ["日", "月", "火", "水", "木", "金", "土"][date.getDay()];
 	const result = `${date.getMonth() + 1}月${date.getDate()}日(${dayOfWeek})`;
@@ -21,10 +21,19 @@ export function getPeriodString(dateStr: Date) {
 export function isDisabled(dateStr: Date) {
 	//引数の日付が今週に含まれるか判定
 	const today = new Date();
-	return getMonday(today) === getMonday(dateStr) ? true : false;
+	return getMondayComp(today) === getMondayComp(dateStr) ? true : false;
 }
 
-function getMonday(d: Date) {
+export function getMonday(d: Date) {
+	//月曜日を算出する
+	const target = new Date(d);
+	const day = target.getDay();
+	const diff = day === 0 ? -6 : 1 - day; // 日曜なら-6、それ以外は1-day
+	target.setDate(target.getDate() + diff);
+	return target; // 比較のためにタイムスタンプを返す
+}
+
+function getMondayComp(d: Date) {
 	//月曜日を算出する
 	const target = new Date(d);
 	target.setHours(0, 0, 0, 0); // 時刻をリセット
--- src/features/calendar/views/CardsList.tsx
+++ src/features/calendar/views/CardsList.tsx
@@ -2,17 +2,27 @@ import Paper from "@mui/material/Paper";
 import { CalendarTypography } from "../../../styles/CalendarTypo";
 import type { mealType } from "../../../types/types";
 import { showMealType } from "../composable/showMealType";
-import { showDateString } from "../composable/showDateString";
+import { getMonday, showDateString } from "../composable/showDateString";
+import { useAtomValue } from "jotai";
+import { dispDate } from "../../../contexts/date";
 
 export const CardsList = () => {
+	const date = useAtomValue(dispDate);
+
 	return (
 		<>
-			<DayCard date="2026-02-16" />
+			{[...Array(7)].map((_, index) => {
+				// 基準日（月曜日）に index (0〜6) を足していく
+				const currentDay = new Date(getMonday(date));
+				currentDay.setDate(getMonday(date).getDate() + index);
+
+				return <DayCard key={index} date={currentDay} />;
+			})}
 		</>
 	);
 };
 
-const DayCard = ({ date }: { date: string }) => {
+const DayCard = ({ date }: { date: Date }) => {
 	return (
 		<Paper sx={{ p: 3, borderRadius: 4 }}>
 			<CalendarTypography role="cardtitle" sx={{ mb: 4 }}>
